7ff4e36ca2d87531af9db46da77500d53aa07dd9
manugee95 <manugeetech@gmail.com>
2023-07-26 12:07:43 +0100

    first commit

---
+++ main/forms.py
@@ -0,0 +1,20 @@
+from django import forms 
+from .models import Contact
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from userprofile.models import Customer
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['full_name', 'email', 'message', 'screenshot']
+
+class CustomerForm(UserCreationForm):
+    class Meta:
+        model = User 
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'pix']
